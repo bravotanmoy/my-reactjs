9ebcd94df55db6caf9ae9c1ca56bbf4963c0090f
Tanmoy Biswas <tonmoy@flightexpert.com>
2024-10-04 10:53:05 +0600

    Clock script

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Clock from './components/Clock';
 function App() {
     return (
         <div>
-             <Clock locale="bn-BD" /> 
-            pooi
+             <Clock /> 
         </div>
     );
 }
