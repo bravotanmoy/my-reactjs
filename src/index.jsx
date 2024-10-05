03f3c8077ff1e2a45ec04735554c4193307994ff
Tanmoy Biswas <tonmoy@flightexpert.com>
2024-10-05 18:27:02 +0600

    11-react Lifting State Up

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <React.StrictMode>
+    <React.StrictMode>
       <App />
-    // </React.StrictMode>
+    </React.StrictMode>
 );
 
